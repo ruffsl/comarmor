cfba567b9d3c5633f6ca30bbef98e5819b9f73f5
ruffsl <roxfoxpox@gmail.com>
2017-09-28 14:47:57 -0700

    Version 0.0.0

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='comarmor',
+    version='0.0.0',
+    packages=['comarmor'],
+    url='https://github.com/ComArmor/comarmor',
+    download_url = 'https://github.com/ComArmor/comarmor/archive/0.0.0.tar.gz',
+    license='',
+    author='',
+    author_email='',
+    description='Like AppArmor, but for Secure Communications'
+)
